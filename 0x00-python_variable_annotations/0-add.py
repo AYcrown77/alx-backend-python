82fd75657392bcb14a9e7fdc1e9f0b88b6f8dbdc
AYcrown77 <aycrown77@gmail.com>
2023-04-02 11:46:13 +0000

    Solve task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Write a type-annotated function add that takes a float a and
+float b as arguments and returns their sum as a float
+"""
+
+
+def add(a: float, b:float) -> float:
+    """Add two numbers and return the result"""
+    return (a + b)
