ce3ffad4fb7e32cb4e7ae4cb8440daf8d10aa302
AYcrown77 <aycrown77@gmail.com>
2023-04-02 11:47:32 +0000

    Solve task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Write a type-annoted function concat that takes 2 strindgs as argument
+and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) ->str:
+    """Concatenate 2 strings"""
+    return str1 + str2
