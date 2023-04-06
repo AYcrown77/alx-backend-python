e4406ba52e8a2ed9a92a55158ac5cf7ffc1abf06
AYcrown77 <aycrown77@gmail.com>
2023-04-06 07:58:41 +0000

    Fix pycodestyle

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Import wait_random from the previous python file that you’ve written and write an async routine called wait_n that takes in 2 int arguments (in this order): n and max_delay."""
+"""Import wait_random from the previous python file"""
 
 
 import asyncio
