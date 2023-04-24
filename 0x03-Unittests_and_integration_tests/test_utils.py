010697f3f20b6d7346af289a8ccbf0f349388eca
AYcrown77 <aycrown77@gmail.com>
2023-04-24 18:25:08 +0100

    Solve task 0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,12 +9,11 @@ from unittest.mock import Mock, patch
 
 class TestAccessNestedMap(unittest.TestCase):
     """test access_nested_map()"""
-     @parameterized.expand([
+    @parameterized.expand([
         nested_map={"a": 1}, path=("a",)
         nested_map={"a": {"b": 2}}, path=("a",)
         nested_map={"a": {"b": 2}}, path=("a", "b")
     ])
-
     def test_access_nested_map(self, map, path, expected):
         """test access_nested_map return value"""
         self.assertEqual(access_nested_map(map, path), expected)
